bd5caecb9634ca9348aa2ca661057df9ec338546
Darshan Patel <darshanpatel@arisglobal.com>
2022-07-14 11:18:25 -0400

    avatar component update

--- src/stories/Avatar.stories.js
+++ src/stories/Avatar.stories.js
@@ -1,21 +1,21 @@
-import { Avatar } from "@mui/material";
+import AvatarComponent from "../components/avatar-component";
 
 export default {
     title: 'Data Display/Avatar',
-    component: Avatar,
+    component: AvatarComponent,
     argTypes: {
-        variant: {
-            options: ['circular', 'rounded', 'square'],
+        type: {
+            options: ['text', 'image'],
             control: { type: 'radio' },
         },
     },
 };
 
-const Template = (args) => <Avatar {...args} />;
+const Template = (args) => <AvatarComponent {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-    variant:'rounded',
-    alt:'Light House',
-    src:"https://picsum.photos/id/870/200/300?grayscale&blur=2",
+    type:'text',
+    avatarText:'Software Tester',
+    src:'',
 }
