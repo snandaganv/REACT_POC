81ab7ef16d0ba8d7819e50bdb67f4f5af1c30ce0
mjatta-agi <mjatta@arisglobal.com>
2022-07-16 14:41:37 -0400

    Add more button examples

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 import Button from "@mui/material/Button";
+import { Stack } from "@mui/material";
+import Save from "@mui/icons-material/Save";
+import { LoadingButton } from "@mui/lab";
 
-export const Defaults = ({ label, onClick, variant, color, size }) => {
+export const Defaults = ({
+  label,
+  onClick,
+  variant,
+  color,
+  size,
+  direction,
+}) => {
   return (
-    <Button onClick={onClick} variant={variant} color={color} size={size}>
-      {label}
-    </Button>
+    <Stack direction={direction} spacing={2}>
+      <Button onClick={onClick} variant={variant} color={color} size={size}>
+        {label}
+      </Button>
+    </Stack>
   );
 };
 
@@ -22,10 +34,158 @@ Defaults.propTypes = {
     "info",
     "warning",
   ]),
+  direction: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse",
+  ]),
 };
 
-Defaults.defaultsProps = {
-  label: "Button",
-  size: "medium",
-  onClick: undefined,
+export const ButtonGroups = ({ color, direction, onClick, size }) => {
+  return (
+    <Stack direction={direction} spacing={2}>
+      <Button variant="text" onClick={onClick} size={size} color={color}>
+        Text
+      </Button>
+      <Button variant="contained" onClick={onClick} size={size} color={color}>
+        Contained
+      </Button>
+      <Button variant="outlined" onClick={onClick} size={size} color={color}>
+        Outlined
+      </Button>
+      <Button disabled variant="contained" size={size}>
+        Disabled
+      </Button>
+    </Stack>
+  );
+};
+
+ButtonGroups.propTypes = {
+  direction: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse",
+  ]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  color: PropTypes.oneOf([
+    "primary",
+    "success",
+    "secondary",
+    "error",
+    "info",
+    "warning",
+  ]),
+  onClick: PropTypes.func,
+};
+
+export const ButtonIcon = ({
+  onClick,
+  label,
+  direction,
+  variant,
+  startIcon,
+  endIcon,
+  size,
+  color,
+}) => {
+  return (
+    <Stack direction={direction} spacing={2}>
+      <Button
+        variant={variant}
+        onClick={onClick}
+        startIcon={startIcon}
+        size={size}
+        color={color}
+      >
+        {label}
+      </Button>
+      <Button
+        variant={variant}
+        onClick={onClick}
+        size={size}
+        color={color}
+        endIcon={endIcon}
+      >
+        Send
+      </Button>
+    </Stack>
+  );
+};
+
+ButtonIcon.propTypes = {
+  direction: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse",
+  ]),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  color: PropTypes.oneOf([
+    "primary",
+    "success",
+    "secondary",
+    "error",
+    "info",
+    "warning",
+  ]),
+  backgroundColor: PropTypes.string,
+  label: PropTypes.string,
+  onClick: PropTypes.func,
+  endIcon: PropTypes.func,
+  startIcon: PropTypes.func,
+};
+
+export const LoadingButtons = ({
+  loading,
+  direction,
+  loadingIndicator,
+  loadingPosition,
+  size,
+  variant,
+  onClick,
+}) => {
+  return (
+    <Stack direction={direction} spacing={2}>
+      <LoadingButton variant={variant} size={size} onClick={onClick}>
+        Submit
+      </LoadingButton>
+      <LoadingButton loading={loading} size={size} variant={variant}>
+        CLick to Sumbit
+      </LoadingButton>
+      <LoadingButton
+        loading
+        loadingIndicator={loadingIndicator}
+        variant={variant}
+        size={size}
+      >
+        Fetch data
+      </LoadingButton>
+      <LoadingButton
+        loading
+        loadingPosition={loadingPosition}
+        size={size}
+        startIcon={<Save />}
+        variant={variant}
+      >
+        Save
+      </LoadingButton>
+    </Stack>
+  );
+};
+
+LoadingButtons.propTypes = {
+  direction: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse",
+  ]),
+  loading: PropTypes.bool,
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  label: PropTypes.string,
+  loadingIndicator: PropTypes.string,
+  loadingPosition: PropTypes.string,
+  onClick: PropTypes.func,
 };
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,16 +1,21 @@
 import { Button } from "@mui/material";
-import Basic from "../components/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
-import Stack from "@mui/material/Stack";
-import { Box } from "@mui/material";
-
-import { Defaults } from "../components/Button";
+import {
+  Defaults,
+  ButtonGroups,
+  ButtonIcon,
+  LoadingButtons,
+} from "../components/Button";
+import Send from "@mui/icons-material/Send";
+import Delete from "@mui/icons-material/Delete";
 
 export default {
   title: "Input/Button",
   component: Button,
   argTypes: {
+    direction: {
+      options: ["row", "row-reverse", "column", "column-reverse"],
+      control: { type: "radio" },
+    },
     variant: {
       options: ["contained", "outlined", "text"],
       control: { type: "radio" },
@@ -19,161 +24,49 @@ export default {
       options: ["small", "medium", "large"],
       control: { type: "radio" },
     },
+    loadingPosition: {
+      options: ["start", "center", "end"],
+      control: { type: "radio" },
+    },
     color: {
       options: ["primary", "success", "secondary", "error", "info", "warning"],
       control: { type: "radio" },
     },
     onClick: { action: "clicked" },
   },
-  direction: {
-    options: ["row", "row-reverse", "column", "column-reverse"],
-    control: { type: "radio" },
-  },
-  parameters: {
-    backgrounds: {
-      default: "Light Theme",
-      values: [{ name: "Dark Theme", value: "#656B73" }],
-    },
-  },
 };
 
 const Template = (args) => <Defaults {...args} />;
+const Groups = (args) => <ButtonGroups {...args} />;
+const Icons = (args) => <ButtonIcon {...args} />;
+const Loading = (args) => <LoadingButtons {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   label: "Button",
-};
-
-export const BasicButtons = ({ direction, onClick }) => {
-  return (
-    <Stack spacing={2} direction={direction}>
-      <Button variant="text" onClick={onClick}>
-        Text
-      </Button>
-      <Button variant="contained" onClick={onClick}>
-        Contained
-      </Button>
-      <Button variant="outlined" onClick={onClick}>
-        Outlined
-      </Button>
-      <Button disabled variant="contained">
-        Disabled
-      </Button>
-    </Stack>
-  );
-};
-BasicButtons.args = {
   direction: "row",
+  variant: "contained",
+  color: "primary",
+  size: "medium",
 };
 
-export const ButtonSizes = ({ direction, onClick }) => {
-  return (
-    <Stack direction={direction} spacing={2}>
-      <Box sx={{ "& button": { m: 1 } }}>
-        {/* <div> */}
-        <Button size="small" onClick={onClick}>
-          Small
-        </Button>
-        <Button size="medium" onClick={onClick}>
-          Medium
-        </Button>
-        <Button size="large" onClick={onClick}>
-          Large
-        </Button>
-        {/* </div>
-        <div> */}
-        <Button variant="outlined" size="small" onClick={onClick}>
-          Small
-        </Button>
-        <Button variant="outlined" size="medium" onClick={onClick}>
-          Medium
-        </Button>
-        <Button variant="outlined" size="large" onClick={onClick}>
-          Large
-        </Button>
-        {/* </div>
-        <div> */}
-        <Button variant="contained" size="small" onClick={onClick}>
-          Small
-        </Button>
-        <Button variant="contained" size="medium" onClick={onClick}>
-          Medium
-        </Button>
-        <Button variant="contained" size="large" onClick={onClick}>
-          Large
-        </Button>
-        {/* </div> */}
-      </Box>
-    </Stack>
-  );
-};
-
-ButtonSizes.args = {
-  direction: "column",
-};
-
-export const ButtonIconLabel = ({ direction, onClick }) => {
-  return (
-    <Stack direction={direction} spacing={2}>
-      <Button variant="outlined" startIcon={<DeleteIcon />} onClick={onClick}>
-        Delete
-      </Button>
-      <Button variant="contained" endIcon={<SendIcon />} onClick={onClick}>
-        Send
-      </Button>
-    </Stack>
-  );
-};
-
-ButtonIconLabel.args = {
+export const ButtonGroup = Groups.bind({});
+ButtonGroup.args = {
   direction: "row",
+  size: "medium",
 };
-
-export const Try = ({ direction, onClick }) => {
-  return (
-    // <Stack direction={direction} spacing={2} sx={{ "& button": { m: 1 } }}>
-    // <Box sx={{ "& button": { m: 1 } }}>
-    //   <Stack direction={direction} spacing={1}>
-    //     <Button variant="contained" size="small" onClick={onClick}>
-    //       Small
-    //     </Button>
-    //     <Button variant="contained" size="medium" onClick={onClick}>
-    //       Medium
-    //     </Button>
-    //     <Button variant="contained" size="large" onClick={onClick}>
-    //       Large
-    //     </Button>
-    //   </Stack>
-    //   <Button variant="contained" size="small" onClick={onClick}>
-    //     Small
-    //   </Button>
-    //   <Button variant="contained" size="medium" onClick={onClick}>
-    //     Medium
-    //   </Button>
-    //   <Button variant="contained" size="large" onClick={onClick}>
-    //     Large
-    //   </Button>
-    //   <Button colorScheme="blue" variant="contained" size="small">
-    //     Button
-    //   </Button>
-    //   <Button colorScheme="blue" variant="contained" size="medium">
-    //     Button
-    //   </Button>
-    //   <Button colorScheme="blue" variant="contained" size="large">
-    //     Button
-    //   </Button>
-    // </Box>
-    <Stack direction={direction} spacing={2}>
-      <Button variant="outlined" size="small" onClick={onClick}>
-        Delete
-      </Button>
-      <Button variant="contained" onClick={onClick}>
-        Send
-      </Button>
-    </Stack>
-  );
-};
-
-Try.args = {
+export const ButtonIcons = Icons.bind({});
+ButtonIcons.args = {
+  label: "Delete",
   direction: "row",
+  endIcon: <Send />,
+  startIcon: <Delete />,
+};
+
+export const LoadingButton = Loading.bind({});
+LoadingButton.args = {
+  direction: "row",
+  loading: true,
+  loadingIndicator: "loading...",
+  loadingPosition: "start",
 };
