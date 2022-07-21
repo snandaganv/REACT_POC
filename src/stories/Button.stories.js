bce365c75b62a636bd64c921ee1a711f0a8f9acb
mjatta-agi <mjatta@arisglobal.com>
2022-07-21 08:57:22 -0400

    refactor Button

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,21 +1,13 @@
-import { Button } from "@mui/material";
-import {
-  Defaults,
-  ButtonGroups,
-  ButtonIcon,
-  LoadingButtons,
-} from "../components/Button";
+import { Button, Stack } from "@mui/material";
 import Send from "@mui/icons-material/Send";
 import Delete from "@mui/icons-material/Delete";
+import Save from "@mui/icons-material/Save";
+import { LoadingButton } from "@mui/lab";
 
 export default {
   title: "Input/Button",
   component: Button,
   argTypes: {
-    direction: {
-      options: ["row", "row-reverse", "column", "column-reverse"],
-      control: { type: "radio" },
-    },
     variant: {
       options: ["contained", "outlined", "text"],
       control: { type: "radio" },
@@ -24,52 +16,81 @@ export default {
       options: ["small", "medium", "large"],
       control: { type: "radio" },
     },
-    loadingPosition: {
-      options: ["start", "center", "end"],
-      control: { type: "radio" },
-    },
     color: {
       options: ["primary", "success", "secondary", "error", "info", "warning"],
       control: { type: "radio" },
     },
-    onClick: { action: "clicked" },
   },
 };
 
-const Template = (args) => <Defaults {...args} />;
-const Groups = (args) => <ButtonGroups {...args} />;
-const Icons = (args) => <ButtonIcon {...args} />;
-const Loading = (args) => <LoadingButtons {...args} />;
+const Template = (args) => <Button {...args}>Button</Button>;
+
+const Basics = (args) => (
+  <Stack spacing={2} direction="row">
+    <Button variant="text">Text</Button>
+    <Button variant="contained">Contained</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Button variant="contained" disabled>
+      Disabled
+    </Button>
+    <Button variant="contained" href="#contained-buttons">
+      Link
+    </Button>
+  </Stack>
+);
+
+const HandingClick = (args) => (
+  <Button
+    onClick={() => {
+      alert("clicked");
+    }}
+  >
+    Click me
+  </Button>
+);
+
+const IconLabelButton = (args) => (
+  <Stack direction="row" spacing={2}>
+    <Button variant="outlined" startIcon={<Delete />}>
+      Delete
+    </Button>
+    <Button variant="contained" endIcon={<Send />}>
+      Send
+    </Button>
+  </Stack>
+);
+
+const Loadings = (args) => (
+  <Stack direction="row" spacing={2}>
+    <LoadingButton loading variant="outlined">
+      Submit
+    </LoadingButton>
+    <LoadingButton loading loadingIndicator="Loading…" variant="outlined">
+      Fetch data
+    </LoadingButton>
+    <LoadingButton
+      loading
+      loadingPosition="start"
+      startIcon={<Save />}
+      variant="outlined"
+    >
+      Save
+    </LoadingButton>
+  </Stack>
+);
 
 export const Default = Template.bind({});
 Default.args = {
   label: "Button",
-  direction: "row",
   variant: "contained",
   color: "primary",
   size: "medium",
 };
 
-export const ButtonGroup = Groups.bind({});
-ButtonGroup.args = {
-  direction: "row",
-  size: "medium",
-};
+export const BasicButton = Basics.bind({});
 
-export const ButtonIcons = Icons.bind({});
-ButtonIcons.args = {
-  label: "Delete",
-  direction: "row",
-  variant: "contained",
-  endIcon: <Send />,
-  startIcon: <Delete />,
-};
+export const HandingClicks = HandingClick.bind({});
 
-export const LoadingButton = Loading.bind({});
-LoadingButton.args = {
-  direction: "row",
-  variant: "contained",
-  loading: true,
-  loadingIndicator: "loading...",
-  loadingPosition: "start",
-};
+export const IconLabelButtons = IconLabelButton.bind({});
+
+export const LoadingButtons = Loadings.bind({});
