7d9480e0f47bf3b9786cf6781025bc1c77534a0e
mjatta-agi <mjatta@arisglobal.com>
2022-07-22 15:11:20 -0400

    Refactor Checkboxes

--- src/stories/CheckBox.stories.js
+++ src/stories/CheckBox.stories.js
@@ -1,11 +1,18 @@
 import React from "react";
-import CheckBoxes from "../components/Checkbox";
-import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+//import CheckBoxes from "../components/Checkbox";
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+} from "@mui/material";
 import { Box } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { FormControl } from "@mui/material";
 
 export default {
   title: "Input/Checkbox",
-  component: CheckBoxes,
+  component: Checkbox,
   argTypes: {
     onclick: {
       action: "checked",
@@ -13,17 +20,75 @@ export default {
   },
 };
 
-const Template = (args) => <CheckBoxes {...args} />;
+export const BasicCheckboxes = ({}) => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  return (
+    <div>
+      <Checkbox {...label} defaultChecked />
+      <Checkbox {...label} />
+      <Checkbox {...label} disabled />
+      <Checkbox {...label} disabled checked />
+    </div>
+  );
+};
 
-export const Basic = Template.bind({});
-Basic.args = {
+BasicCheckboxes.args = {
   label: "Label",
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
-  label: "Disabled",
-  disabled: true,
+export const Labels = ({ label }) => {
+  return (
+    <FormGroup>
+      <FormControlLabel control={<Checkbox defaultChecked />} label={label} />
+      <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+    </FormGroup>
+  );
+};
+Labels.args = {
+  label: "Label",
+};
+
+export const Sizes = ({ ...props }) => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  return (
+    <div>
+      <Checkbox {...label} defaultChecked size="small" />
+      <Checkbox {...label} defaultChecked />
+      <Checkbox
+        {...label}
+        defaultChecked
+        sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+      />
+    </div>
+  );
+};
+Sizes.args = {
+  label: "label",
+};
+
+export const Colors = ({ ...props }) => {
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+  return (
+    <div>
+      <Checkbox {...label} defaultChecked />
+      <Checkbox {...label} defaultChecked color="secondary" />
+      <Checkbox {...label} defaultChecked color="success" />
+      <Checkbox {...label} defaultChecked color="default" />
+      <Checkbox
+        {...label}
+        defaultChecked
+        sx={{
+          color: blue[800],
+          "&.Mui-checked": {
+            color: blue[600],
+          },
+        }}
+      />
+    </div>
+  );
+};
+Colors.args = {
+  label: "label",
 };
 
 export const Group = ({ row, onclick, ...props }) => {
@@ -64,7 +129,7 @@ export const Inderterminate = ({ label, row, onClick, ...props }) => {
       <FormControlLabel
         label="Child 1"
         control={
-          <CheckBoxes
+          <Checkbox
             checked={checked[0]}
             onChange={handleChange2}
             onClick={onClick}
@@ -75,7 +140,7 @@ export const Inderterminate = ({ label, row, onClick, ...props }) => {
       <FormControlLabel
         label="Child 2"
         control={
-          <CheckBoxes
+          <Checkbox
             checked={checked[1]}
             onChange={handleChange3}
             onClick={onClick}
@@ -88,14 +153,12 @@ export const Inderterminate = ({ label, row, onClick, ...props }) => {
   return (
     <div>
       <FormControlLabel
-        label={label}
+        label="Parent"
         control={
-          <CheckBoxes
+          <Checkbox
             checked={checked[0] && checked[1]}
             indeterminate={checked[0] !== checked[1]}
             onChange={handleChange1}
-            onClick={onClick}
-            {...props}
           />
         }
       />
@@ -106,4 +169,41 @@ export const Inderterminate = ({ label, row, onClick, ...props }) => {
 
 Inderterminate.args = {
   label: "Parent",
+};
+
+export const LabelsPlacement = ({ ...props }) => {
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Label placement</FormLabel>
+      <FormGroup aria-label="position" row>
+        <FormControlLabel
+          value="top"
+          control={<Checkbox />}
+          label="Top"
+          labelPlacement="top"
+        />
+        <FormControlLabel
+          value="start"
+          control={<Checkbox />}
+          label="Start"
+          labelPlacement="start"
+        />
+        <FormControlLabel
+          value="bottom"
+          control={<Checkbox />}
+          label="Bottom"
+          labelPlacement="bottom"
+        />
+        <FormControlLabel
+          value="end"
+          control={<Checkbox />}
+          label="End"
+          labelPlacement="end"
+        />
+      </FormGroup>
+    </FormControl>
+  );
+};
+LabelsPlacement.args = {
+  label: "label",
 };
