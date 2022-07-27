fca1d357afe1f27258a6560f30d9a3a07c1c8cff
Darshan Patel <darshanpatel@arisglobal.com>
2022-07-27 14:40:05 -0400

    changes for using one component for all icons
    created a render propd function for passing any icons component in it and promoting reusability with one parent component

--- src/components/menu-icons-component/index.js
+++ src/components/menu-icons-component/index.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { PropTypes } from "prop-types";
+
 import MenuIcon from '@mui/icons-material/Menu';
-//import MenuBookIcon from '@mui/icons-material/MenuBook';
 
 const MenuIconsComponent = (props) => {
-
    
   return (
     <>
-      <MenuIcon {...props}></MenuIcon>
+      <div>{props.childComponent(props)}</div>
 
     </>
   );
