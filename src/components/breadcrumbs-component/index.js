57bfe3536426911655867462a0f3220053251632
Darshan Patel <darshanpatel@arisglobal.com>
2022-07-29 11:46:34 -0400

    added changes for breadcrumb component
    created a resuable component of breadcrumd navigation

--- src/components/breadcrumbs-component/index.js
+++ src/components/breadcrumbs-component/index.js
@@ -4,55 +4,29 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 
 const BreadcrumbsComponent = (props) => {
-
-  const { type, label, fullWidth,autoFocus,size,defaultValue,multiline,
-    helperText,
-    row,
-    color,
-    variant,
-    error,
-    disabled} = props;
-  const onChanged =(changed)=> {
-    console.log(changed)
+  const onClicked =(event)=> {
+    console.log(event);
   }
   
   return (
     <>
-      <Breadcrumbs label={label} fullWidth={fullWidth} type={type} autoFocus={autoFocus} size={size} defaultValue={defaultValue}  helperText={helperText} row={row} color={color} variant ={variant} error={error} disabled={disabled} onChange={onChanged}></TextField>
+      <Breadcrumbs {...props} onClick={onClicked}></Breadcrumbs>
     </>
   );
 }
 
 BreadcrumbsComponent.prototype = {
-  label:PropTypes.string,
-  type:PropTypes.string,
-  autoFocus: PropTypes.boolean,
-  size:PropTypes.string,
-  fullWidth:PropTypes.boolean,
-  multiline:PropTypes.boolean,
-  helperText:PropTypes.string,
-  rows:PropTypes.number,
-  color:PropTypes.string,
-  variant:PropTypes.string,
-  error:PropTypes.boolean,
-  disabled:PropTypes.boolean,
-  defaultValue:PropTypes.string
+  expandText:PropTypes.string,
+  itemsAfterCollapse: PropTypes.number,
+  itemsBeforeCollapse: PropTypes.number,
+  maxItems: PropTypes.number,
 };
 
 BreadcrumbsComponent.defaultProps = {
-  label:'',
-  type:'text',
-  autoFocus: true,
-  size:'',
-  fullWidth:true,
-  multiline:false,
-  helperText:'',
-  rows:3,
-  color:'',
-  variant:'',
-  error:false,
-  disabled:false,
-  defaultValue:'Please enter name'
+  expandText:'Show Link',
+  itemsAfterCollapse: 2,
+  itemsBeforeCollapse: 2,
+  maxItems: 6,
 };
 
 
--- src/stories/Breadcrumbs.stories.js
+++ src/stories/Breadcrumbs.stories.js
@@ -1,54 +1,35 @@
+import { Breadcrumbs } from "@mui/material";
 import BreadcrumbsComponent from "../components/breadcrumbs-component";
+import Link from '@mui/material/Link';
 
 export default {
     title: 'Navigation/Breadcrumbs',
     component: BreadcrumbsComponent,
     argTypes: {
-        autoFocus: true,
-        defaultValue: 'TextField',
-        disabled: {
-            options: [true, false],
-            control: { type: 'select' },
-        },
-        error: {
-            options: [true, false],
-            control: { type: 'select' },
-        },
-        variant: {
-            options: ['outlined', 'filled', 'standard'],
-            control: { type: 'radio' },
-        },
-        color: {
-            options: ['primary', 'secondary', 'error', 'success', 'warning', 'info'],
-            control: { type: 'radio' },
-        },
-        type: {
-            options: ['text', 'password', 'number'],
-            control: { type: 'radio' },
-        },
-        size: {
-            options: ['small', 'medium'],
-            control: { type: 'radio' },
-        },
-        onChange: {action: 'onChange'},
+        expandText:'Show Link',
+        itemsAfterCollapse: 2,
+        itemsBeforeCollapse: 2,
+        maxItems: 6,
+        onClick: {action: 'onClick'},
     },
 };
 
-const Template = (args) => <BreadcrumbsComponent {...args} onChange={args.onChange} />;
+const Template = (args) => <BreadcrumbsComponent {...args} onClick={args.onClick}>
+    <Link  underline="hover" color="inherit" href="/">MUI</Link>
+    <Link
+    underline="hover"
+    color="inherit"
+    href="/material-ui/getting-started/installation/"
+  >
+    Core
+  </Link>
+  
+    </BreadcrumbsComponent>;
 
 export const Default = Template.bind({});
 Default.args = {
-    label:'UserName',
-    type:'text',
-    autoFocus: true,
-    size:'small',
-    fullWidth:true,
-    multiline:false,
-    helperText:'Username is mandatory',
-    rows:3,
-    color:'primary',
-    variant:'outlined',
-    error:false,
-    disabled:false,
-    defaultValue:'Please enter name'
+    expandText:'Show Link',
+    itemsAfterCollapse: 2,
+    itemsBeforeCollapse: 2,
+    maxItems: 6,
 }
