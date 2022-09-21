b4607fc028b49b4335369c814093d0dad58bf202
Darshan Patel <darshanpatel@arisglobal.com>
2022-09-21 14:12:22 -0400

    added all the colors customising theme

--- .storybook/muiTheme.js
+++ .storybook/muiTheme.js
@@ -12,15 +12,47 @@ const muiTheme = createTheme({
 
     },
     secondary: {
-      main: variables.secondPrimary,
-      light:variables.secondLight,
-      dark: variables.secondDark,
+      main: variables.secondaryPrimary,
+      light:variables.secondaryLight,
+      dark: variables.secondaryDark,
       
+  },
+  pastel: {
+    main: variables.pastelPrimary,
+    light:variables.pastelLight,
+    dark: variables.pastelDark,
+  },
+  yellow: {
+    main: variables.yellowPrimary,
+    light:variables.yellowLight,
+    dark: variables.yellowDark,
+  },
+  grey: {
+    main: variables.greyPrimary,
+    light:variables.greyLight,
+    dark: variables.greyDark,
   },
  
     error: {
         main: variables.errorMain,
+        light: variables.errorLight,
+        dark: variables.errorDark
     },
+    warning: {
+      main:variables.warningMain,
+      light: variables.warningLight,
+      dark: variables.warningDark
+    },
+    info: {
+      main:variables.infoMain,
+      light: variables.infoLight,
+      dark: variables.infoDark
+    },
+    success: {
+      main: variables.successMain,
+      light: variables.successLight,
+      dark: variables.successDark
+    }
   },
 });
 
