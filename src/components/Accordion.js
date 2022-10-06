19080ec2e07bc934aed276cec0246ba9450232d4
mjatta-agi <mjatta@arisglobal.com>
2022-10-05 20:22:01 -0400

    Add Accordion component to storybook

---
+++ src/components/Accordion.js
@@ -0,0 +1,175 @@
+import * as React from "react";
+import { PropTypes } from "prop-types";
+import { styled } from "@mui/material/styles";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+
+export const AccordionBasic = (props) => {
+  const { data } = props;
+  return (
+    <div>
+      {data.map((item, index) => {
+        return (
+          <>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={"panel-content" + index}
+                id={"panel-header" + index}
+              >
+                <Typography key={"header_" + index}>{item.heading}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography key={"content" + index}>{item.content}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+AccordionBasic.prototype = {
+  data: PropTypes.array,
+};
+
+AccordionBasic.defaultProps = {
+  data: [],
+};
+
+export const AccordionControlled = (props) => {
+  const { data } = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      {data.map((item, index) => {
+        return (
+          <>
+            <Accordion
+              expanded={expanded === "panel" + index}
+              onChange={handleChange("panel" + index)}
+            >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={"panel-content" + index}
+                id={"panel-header" + index}
+              >
+                <Typography
+                  sx={{ width: "33%", flexShrink: 0 }}
+                  key={"header_" + index}
+                >
+                  {item.heading}
+                </Typography>
+                <Typography
+                  sx={{ color: "text.secondary" }}
+                  key={"secondary" + index}
+                >
+                  {item.secondaryHeading}
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography key={"content" + index}>{item.content}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+AccordionControlled.prototype = {
+  data: PropTypes.array,
+};
+
+AccordionControlled.defaultProps = {
+  data: [],
+};
+
+export const AccordionCustomization = (props) => {
+  const { data } = props;
+  const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} square {...props} />
+  ))(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+  }));
+
+  const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+      expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255, 255, 255, .05)"
+        : "rgba(0, 0, 0, .03)",
+    flexDirection: "row-reverse",
+    "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+      transform: "rotate(90deg)",
+    },
+    "& .MuiAccordionSummary-content": {
+      marginLeft: theme.spacing(1),
+    },
+  }));
+
+  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+    padding: theme.spacing(2),
+    borderTop: "1px solid rgba(0, 0, 0, .125)",
+  }));
+  const [expanded, setExpanded] = React.useState("panel");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      {data.map((item, index) => {
+        return (
+          <>
+            <Accordion
+              expanded={expanded === "panel" + index}
+              onChange={handleChange("panel" + index)}
+            >
+              <AccordionSummary
+                aria-controls={"panel-content" + index}
+                id={"panel-header" + index}
+              >
+                <Typography key={"header_" + index}>{item.heading}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography key={"content" + index}>{item.content}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+AccordionCustomization.prototype = {
+  data: PropTypes.array,
+};
+
+AccordionCustomization.defaultProps = {
+  data: [],
+};
---
+++ src/stories/Accordion.stories.js
@@ -0,0 +1,76 @@
+import {
+  AccordionBasic,
+  AccordionControlled,
+  AccordionCustomization,
+} from "../components/Accordion";
+
+export default {
+  title: "Surfaces/Accordion",
+};
+
+export const AccordionBasics = AccordionBasic.bind({});
+AccordionBasics.args = {
+  data: [
+    {
+      heading: "Accordion 1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      heading: "Accordion 2",
+      content:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    },
+    {
+      heading: "Accordion 3",
+      content:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ],
+};
+
+export const AccordionsControlled = AccordionControlled.bind({});
+AccordionsControlled.args = {
+  data: [
+    {
+      heading: "General settings",
+      secondaryHeading: "I am an accordion",
+      content:
+        "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget maximus est, id dignissim quam.",
+    },
+    {
+      heading: "User",
+      secondaryHeading: "You are currently not an owener",
+      content:
+        "Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.",
+    },
+    {
+      heading: "Advanced settings",
+      secondaryHeading:
+        "Filtering has been entirely disabled for whole web server",
+      content:
+        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ],
+};
+
+export const AccordionCustomizations = AccordionCustomization.bind({});
+AccordionCustomizations.args = {
+  data: [
+    {
+      heading: "Collapsible Group Item #1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+    },
+    {
+      heading: "Collapsible Group Item #2",
+      content:
+        "Consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget",
+    },
+    {
+      heading: "Collapsible Group Item #3",
+      content:
+        "Uspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget",
+    },
+  ],
+};
