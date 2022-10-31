1c34081f7f900df24b018b7d80a0cf9cb56ac3fd
Darshan Patel <darshanpatel@arisglobal.com>
2022-10-31 09:54:36 -0400

    Implementation for Tree view component with dynamic data

---
+++ src/stories/TreeView.stories.js
@@ -0,0 +1,18 @@
+import TreeViewComponent from "../components/tree-view-component";
+
+export default {
+    title: 'Feedback/TreeList',
+    component: TreeViewComponent,
+    argTypes: {
+        
+    },
+};
+
+const Template = (args) => <TreeViewComponent {...args}></TreeViewComponent>;
+
+export const Default = Template.bind({});
+
+Default.args = {
+ treeViwList: [{'itemName':'Applicationss',nodeId : 1,items:[{'itemName':'Calendar',nodeId:2}]},{'itemName':'Documents',nodeId: 5,items:[{'itemName':'OseS',nodeId:4},{'itemName':'MU',nodeId:8,items:[{'itemName':'index.js',nodeId:6}]}]}],
+ ariaLabel: 'tree view list'
+}
