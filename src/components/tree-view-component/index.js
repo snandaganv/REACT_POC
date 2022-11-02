48fabae26dec745f07b51d1d47ff2b7620a48d6e
Darshan Patel <darshanpatel@arisglobal.com>
2022-11-02 10:53:19 -0400

    changes incorporated for select and expand all and icons configuration for expand and collapse

--- src/components/tree-view-component/index.js
+++ src/components/tree-view-component/index.js
@@ -5,12 +5,59 @@ import muiTheme from '../../../.storybook/muiTheme';
 import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { Icon } from "@mui/material";
 import GridItemRecursive from './GridItemRecursive';
+let result = [];
 
+function loop(arr) {
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].items instanceof Array) {
+      result.push(arr[i].nodeId);
+      loop(arr[i].items);
+    }
+  }
+  return result;    
+}
 
+let allIds = [];
+
+function loopAll(arr) {
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].items instanceof Array) {
+      allIds.push(arr[i].nodeId);
+      loopAll(arr[i].items);
+    }
+    else {
+      allIds.push(arr[i].nodeId)
+    }
+  }
+  return result;    
+}
 const TreeViewComponent = (props) => {
-  const {treeViwList,ariaLabel} = props;
+  const {treeViwList,ariaLabel,endIcon,expandIcon,collapseIcon} = props;
+  const nodeIdAll = loop(treeViwList);
+  const nodeIdAlls = loopAll(treeViwList);
+
+  const [expanded, setExpanded] = React.useState([]);
+  const [selected, setSelected] = React.useState([]);
+
+
+
+const handleExpandClick = () => {
+  setExpanded((oldExpanded) =>
+    oldExpanded.length === 0 ? nodeIdAll : [],
+  );
+};
+
+const handleSelectClick = () => {
+  setSelected((oldSelected) =>
+    oldSelected.length === 0 ? nodeIdAlls : [],
+  );
+};
   
 
 
@@ -20,12 +67,23 @@ const TreeViewComponent = (props) => {
   return (
     <>
     <ThemeProvider theme={muiTheme}>
-
+    <Box sx={{ mb: 1 }}>
+        <Button onClick={handleExpandClick}>
+          {expanded.length === 0 ? 'Expand all' : 'Collapse all'}
+        </Button>
+        <Button onClick={handleSelectClick}>
+          {selected.length === 0 ? 'Select all' : 'Unselect all'}
+        </Button>
+      </Box>
     <TreeView
       aria-label={ariaLabel}
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
+      defaultCollapseIcon={<Icon fontSize="small">{collapseIcon}</Icon>}
+      defaultExpandIcon={<Icon fontSize="small">{expandIcon}</Icon>}
+      defaultEndIcon={<Icon fontSize="small">{endIcon}</Icon>}
       sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+      expanded={expanded}
+      selected={selected}
+      multiSelect
     >
 
       <GridItemRecursive familyTree={treeViwList}/>
@@ -42,11 +100,17 @@ const TreeViewComponent = (props) => {
 TreeViewComponent.prototype = {
   treeViwList: PropTypes.any,
   ariaLabel: PropTypes.string,
+  endIcon:PropTypes.string,
+  expandIcon:PropTypes.string,
+  collapseIcon:PropTypes.string,
 };
 
 TreeViewComponent.defaultProps = {
   treeViwList: [{'itemName':'Application',nodeId : 1,items:[{'itemName':'Calendar',nodeId:2}]},{'itemName':'Documents',nodeId: 5,items:[{'itemName':'OseS',nodeId:4},{'itemName':'MU',nodeId:8,items:[{'itemName':'index.js',nodeId:6}]}]}],
-  ariaLabel: 'tree view list'
+  ariaLabel: 'tree view list',
+  endIcon:'close_square',
+  expandIcon:'expand_more_icon',
+  collapseIcon:'chevron_right_icon',
 };
 
 
--- src/stories/TreeView.stories.js
+++ src/stories/TreeView.stories.js
@@ -14,5 +14,8 @@ export const Default = Template.bind({});
 
 Default.args = {
  treeViwList: [{'itemName':'Applicationss',nodeId : 1,items:[{'itemName':'Calendar',nodeId:2}]},{'itemName':'Documents',nodeId: 5,items:[{'itemName':'OseS',nodeId:4},{'itemName':'MU',nodeId:8,items:[{'itemName':'index.js',nodeId:6}]}]}],
- ariaLabel: 'tree view list'
+ ariaLabel: 'tree view list',
+ endIcon:'close_square',
+ expandIcon:'expand_more_icon',
+ collapseIcon:'chevron_right_icon',
 }
