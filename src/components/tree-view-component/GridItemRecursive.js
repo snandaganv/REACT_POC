193569b228b231904c663824025f9c6c46f1f9f3
arpanapatil-agi <arpana.patil@arisglobal.com>
2022-11-24 17:55:58 +0530

    updated fragment

--- src/components/tree-view-component/GridItemRecursive.js
+++ src/components/tree-view-component/GridItemRecursive.js
@@ -1,30 +1,30 @@
 import TreeItem from '@mui/lab/TreeItem';
 import * as React from 'react';
-const GridItemRecursive =(props) => {
-    const {familyTree} = props;
-    return (
-        <>
+const GridItemRecursive = (props) => {
+  const { familyTree } = props;
+  return (
+    <React.Fragment>
 
       {
         familyTree.map((child) => {
-            if(child.items) {
-                return (
-                    <TreeItem nodeId={child.nodeId} label={child.itemName}>
-                    <GridItemRecursive familyTree={child.items}/>
-                    </TreeItem>
-                );
-            }
-            else {
-                return (
-                    <>
-                      <TreeItem nodeId={child.nodeId} label={child.itemName} />
-                    </>
-                  );
-            }
-          
+          if (child.items) {
+            return (
+              <TreeItem nodeId={child.nodeId} label={child.itemName}>
+                <GridItemRecursive familyTree={child.items} />
+              </TreeItem>
+            );
+          }
+          else {
+            return (
+              <React.Fragment>
+                <TreeItem nodeId={child.nodeId} label={child.itemName} />
+              </React.Fragment>
+            );
+          }
+
         })
       }
-        </>
-    )
+    </React.Fragment>
+  )
 }
 export default GridItemRecursive;
