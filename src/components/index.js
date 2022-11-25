929818d871bbbd5e5d99a3f4df76997e809fdbd4
arpanapatil-agi <arpana.patil@arisglobal.com>
2022-11-25 15:37:13 +0530

    Revert "Plat-1708-Drawer component"
    This reverts commit aedceb8da358a8102a26db9c3d8ec7998a9c500a.

--- src/components/index.js
+++ src/components/index.js
@@ -202,16 +202,3 @@ export {
   SnackbarTransition as SnackbarTransitions,
   SnackbarFloatingsActionButtons as SnackbarFloatingActionButton,
 } from "./Snackbar";
-
-export {
-  DrawersTemporary as DrawerTemporary,
-  DrawersSwipeableTemporary as DrawerTemporarySwipeable,
-  DrawersSwipeableEdge as DrawerEdgeSwipeable,
-  DrawersResponsive as DrawerResponsive,
-  DrawersPersistentLeft as DrawerLeftPersistent,
-  DrawersPersistentRight as DrawerRightPersistent,
-  DrawersMini as DrawerMini,
-  DrawersPermanentLeft as DrawerPermanentLeft,
-  DrawersPermanentRight as DrawerPermanentRight,
-  DrawersClipped as DrawerClipped
-} from "./drawer-component";
