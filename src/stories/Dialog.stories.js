e3627378aa3b6b45d41e23a1e9ca23e96056faf4
arpanapatil-agi <arpana.patil@arisglobal.com>
2022-11-29 14:23:02 +0530

    code cleanup ,removed unwanted decl

--- src/stories/Dialog.stories.js
+++ src/stories/Dialog.stories.js
@@ -1,16 +1,15 @@
 import Dialog from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import {
-    SimpleDialogs,
-    AlertDialogs,
-    AlertDialogSlides,
-    FormDialogs,
-    CustomizedDialogs,
-    FullScreenDialogs,
-    MaxWidthDialogs,
-    ConfirmationDialogs,
-    DraggableDialogs,
-    ScrollDialogs
+    DialogsSimple,
+    DialogsAlert,
+    DialogsSlideAlert,
+    DialogsForm,
+    DialogsCustomized,
+    DialogsFullScreen,
+    DialogsMaxWidth,
+    DialogsConfirmation,
+    DialogsScroll
 } from "../components/dialog-component";
 
 
@@ -20,21 +19,18 @@ export default {
 };
 
 
-export const SimpleDialog = SimpleDialogs.bind({});
-SimpleDialog.args = {
+const DialogsSimpleTemplate = (args) => <DialogsSimple {...args}></DialogsSimple>
+export const DialogSimple = DialogsSimpleTemplate.bind({});
+DialogSimple.args = {
     label: "Open simple dialog",
-    //dialogheader: [{ label: "Set backup account" }],
     fullScreen: false,
     fullWidth: false,
-    //divider: false,
     disableEscapeKeyDown: false,
-
-
-
 };
 
-export const AlertDialog = AlertDialogs.bind({});
-AlertDialog.args = {
+const DialogsAlertTemplate = (args) => <DialogsAlert {...args}></DialogsAlert>
+export const DialogAlert = DialogsAlertTemplate.bind({});
+DialogAlert.args = {
     label: " Open alert dialog",
     disableSpacing: false,
     dividers: false,
@@ -44,23 +40,26 @@ AlertDialog.args = {
     alerttextagree: "Agree",
 
 };
-export const AlertDialogSlide = AlertDialogSlides.bind({});
-AlertDialogSlide.args = {
+const DialogsSlideAlertTemplate = (args) => <DialogsSlideAlert {...args}></DialogsSlideAlert>
+export const DialogSlideAlert = DialogsSlideAlertTemplate.bind({});
+DialogSlideAlert.args = {
     label: "  Slide in alert dialog",
     labelledby: "Use Google's location service?",
     describedby: " Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.",
     alerttextdisagree: "Disagree",
     alerttextagree: "Agree",
 };
-export const FormDialog = FormDialogs.bind({});
-FormDialog.args = {
+const DialogsFormTemplate = (args) => <DialogsForm {...args}></DialogsForm>
+export const DialogForm = DialogsFormTemplate.bind({});
+DialogForm.args = {
     label: "Open form dialog  ",
     alerttextdisagree: "Cancel",
     alerttextagree: "Subscribe",
     describedby: "To subscribe to this website, please enter your email address here. We will send updates occasionally."
 };
-export const CustomizedDialog = CustomizedDialogs.bind({});
-CustomizedDialog.args = {
+const DialogsCustomizedTemplate = (args) => <DialogsCustomized {...args}></DialogsCustomized>
+export const DialogCustomized = DialogsCustomizedTemplate.bind({});
+DialogCustomized.args = {
     label: "Open dialog",
     dialogtypo: [
         { label: "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta acconsectetur ac, vestibulum at eros." },
@@ -71,28 +70,28 @@ CustomizedDialog.args = {
     submit: "Save Changes",
 
 }
-export const FullScreenDialog = FullScreenDialogs.bind({});
-FullScreenDialog.args = {
+const DialogsFullScreenTemplate = (args) => <DialogsFullScreen {...args}></DialogsFullScreen>
+export const DialogFullScreen = DialogsFullScreenTemplate.bind({});
+DialogFullScreen.args = {
     label: "Open full-screen dialog",
 };
-export const MaxWidthDialog = MaxWidthDialogs.bind({});
-MaxWidthDialog.args = {
+const DialogsMaxWidthTemplate = (args) => <DialogsMaxWidth {...args}></DialogsMaxWidth>
+export const DialogMaxWidth = DialogsMaxWidthTemplate.bind({});
+DialogMaxWidth.args = {
     label: "Open max-width dialog",
     content: "You can set my maximum width and whether to adapt or not.",
     selectoptions: [{ value: 'xs', label: 'xs' }, { value: 'sm', label: 'sm' }, { value: 'md', label: 'md' }, { value: 'lg', label: 'lg' }, { value: 'xl', label: 'xl' }],
 };
 
-export const ConfirmationDialog = ConfirmationDialogs.bind({});
-ConfirmationDialog.args = {
+const DialogsConfirmationTemplate = (args) => <DialogsConfirmation {...args}></DialogsConfirmation>
+export const DialogConfirmation = DialogsConfirmationTemplate.bind({});
+DialogConfirmation.args = {
     options: [{ option: 'None' }, { option: 'Atria' }, { option: 'Callisto' }, { option: 'Dione' }, { option: 'Ganymede' }, { option: 'Hangouts Call' }, { option: 'Luna' }, { option: 'Oberon' }, { option: 'Phobos' }, { option: 'Pyxis' }, { option: 'Sedna' }, { option: 'Titania' }, { option: 'Triton' }, { option: 'Umbriel' },],
 };
-export const DraggableDialog = DraggableDialogs.bind({});
-DraggableDialog.args = {
-    label: "Open draggable dialog",
-    disableSpacing: false,
-};
-export const ScrollDialog = ScrollDialogs.bind({});
-ScrollDialog.args = {
+
+const DialogsScrollTemplate = (args) => <DialogsScroll {...args}></DialogsScroll>
+export const DialogScroll = DialogsScrollTemplate.bind({});
+DialogScroll.args = {
     label: "scroll=paper",
     disableSpacing: false,
     alerttextdisagree: "Cancel",
