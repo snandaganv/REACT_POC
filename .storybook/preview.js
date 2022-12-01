882e9a7659b90de7730b0f81bf80f15125cda07e
Shreyas H J <106137416+shreyashj-agi@users.noreply.github.com>
2022-12-01 18:44:26 +0530

    PLAT-2005 Add ArisGlobal Logo back to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import { create } from "@storybook/theming";
+import ArisGlobalLogoDark from "../src/assets/images/ArisGlobalLogoDark.svg";
+import ArisGlobalLogoLight from "../src/assets/images/ArisGlobalLogoLight.svg";
+
+const lightTheme = create({
+  base: "light",
+  brandImage: ArisGlobalLogoLight,
+});
+
+const darkTheme = create({
+  base: "dark",
+  brandImage: ArisGlobalLogoDark,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +19,10 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    dark: darkTheme,
+    light: lightTheme,
+    stylePreview: true,
   },
 };
